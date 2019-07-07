82279ba9b6d6718710893b1ea9f7d6a5e29c43f3
jeffyactive <jeff@reelyactive.com>
2019-07-07 10:17:17 -0400

    Updated interface to implement all features

--- js/app.js
+++ js/app.js
@@ -10,7 +10,19 @@ const DEFAULT_SERVER_URL = 'http://localhost:3001/';
 
 // DOM elements
 let storyUrl = document.querySelector('#storyUrl');
+let serverUrl = document.querySelector('#serverUrl');
+let deviceId = document.querySelector('#deviceId');
+let identifierHeader = document.querySelector('#identifierHeader');
+let associationsTable = document.querySelector('#associationsTable');
+let url = document.querySelector('#url');
+let tags = document.querySelector('#tags');
+let directory = document.querySelector('#directory');
+let position = document.querySelector('#position');
 let storyJson = document.querySelector('#storyJson');
+
+
+// Variables
+let associationsServerUrl = DEFAULT_SERVER_URL;
 
 
 // Handle story button click
@@ -24,4 +36,43 @@ storyButton.addEventListener('click', function() {
     storyUrl.value = null;
     storyUrl.setAttribute('placeholder', 'Enter a valid URL');
   }
+});
+
+
+// Handle server button click
+serverButton.addEventListener('click', function() {
+  if(serverUrl.value && (serverUrl.value.indexOf('http') === 0)) {
+    associationsServerUrl = serverUrl.value;
+  }
+  else {
+    serverUrl.value = null;
+    serverUrl.setAttribute('placeholder', 'Enter a valid URL');
+  }
+});
+
+
+// Handle device button click
+deviceButton.addEventListener('click', function() {
+  identifierHeader.textContent = deviceId.value;
+  identifierHeader.hidden = false;
+  associationsTable.hidden = true;
+
+  cormorant.retrieveAssociations(deviceId.value, false, function(associations) {
+    if(associations) {
+      url.textContent = associations.url || '';
+      tags.textContent = associations.tags || '';
+      directory.textContent = associations.directory || '';
+      position.textContent = associations.position || '';
+      associationsTable.hidden = false;
+
+      if(associations.hasOwnProperty('url')) {
+        cormorant.retrieveStory(associations.url, function(story) {
+          storyJson.textContent = JSON.stringify(story, null, 2);
+        });
+      }
+    }
+    else {
+      // TODO: no associations found
+    }
+  });
 });
