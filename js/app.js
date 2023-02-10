1882c6f34075f77c64ee00e59da270b22acd3b97
jeffyactive <jeff@reelyactive.com>
2023-02-10 16:04:31 -0500

    Display HTTP response status code

--- js/app.js
+++ js/app.js
@@ -6,11 +6,13 @@
 
 // Constant definitions
 const DEFAULT_URL = 'https://reelyactive.github.io/cormorant/';
+const HTTP_STATUS_OK = 200;
 
 
 // DOM elements
 let storyUrl = document.querySelector('#storyUrl');
 let storyJson = document.querySelector('#storyJson');
+let storyStatus = document.querySelector('#storyStatus');
 
 
 storyUrl.value = DEFAULT_URL;
@@ -20,8 +22,15 @@ storyUrl.value = DEFAULT_URL;
 storyButton.addEventListener('click', () => {
   storyJson.textContent = '';
   if(storyUrl.value && (storyUrl.value.indexOf('http') === 0)) {
-    cormorant.retrieveStory(storyUrl.value, (story) => {
+    cormorant.retrieveStory(storyUrl.value, (story, status) => {
       storyJson.textContent = JSON.stringify(story, null, 2);
+      storyStatus.textContent = status;
+      if(status === HTTP_STATUS_OK) {
+        storyStatus.setAttribute('class', 'text-success');
+      }
+      else {
+        storyStatus.setAttribute('class', 'text-danger');
+      }
     });
   }
   else {
